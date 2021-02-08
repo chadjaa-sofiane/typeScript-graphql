cf405e7f9d096ab8e3c887d97e9c665d2944cbbf
chadjaa-sofiane <67962574+chadjaa-sofiane@users.noreply.github.com>
2021-02-08 13:39:34 -0500

    add mongod

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
 import express, { Application } from "express";
-import { buildSchema } from "type-graphql"
+import { buildSchema } from "type-graphql";
 import { userInfo } from "./resolvers/userInfo";
+import mongoose from "mongoose";
+import { config } from "dotenv";
+
+config();
 (async () => {
+  mongoose.connect(`mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.d12nl.mongodb.net/${process.env.HOST_URL}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(()=>console.log("mongodb connect succesfuly "))
+  .catch(()=>console.log("mongodb cannot connect !!!"))
   const app: Application = express();
   const appApollo = new ApolloServer({
     schema: await buildSchema({
